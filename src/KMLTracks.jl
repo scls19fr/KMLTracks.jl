5181ccaad3aad6dbfaa47ed8e6d81775b8a9cc12
Sébastien Celles <s.celles@gmail.com>
2019-11-16 10:14:23 +0100

    docstrings

--- src/KMLTracks.jl
+++ src/KMLTracks.jl
@@ -1,3 +1,10 @@
+"""
+Module for working with [Keyhole Markup Language (KML) file format](https://en.wikipedia.org/wiki/Keyhole_Markup_Language).
+
+Be aware that this module only support a part of gx:Track extension only.
+
+Have a look at https://developers.google.com/kml/documentation for more information.
+"""
 module KMLTracks
 
 using Dates
@@ -6,9 +13,27 @@ using TimeZones
 
 export read_kml_file, parse_kml_string
 
+
+"""
+Constant defining KML Format version
+"""
+const KML_VERSION = "2.2"
+
+"""
+Constant dictionary defining default namespaces
+"""
+const KML_NS = Dict(
+    "" => "http://www.opengis.net/kml/2.2",
+    "gx" => "http://www.google.com/kml/ext/2.2"
+)
+
 const dt0 = ZonedDateTime(0, tz"UTC")
 
+"""
+    KMLTrackPoint(time::ZonedDateTime, longitude::Float64, latitude::Float64, altitude::Float64)
 
+Point in a KML Track.
+"""
 struct KMLTrackPoint
     time::ZonedDateTime
     longitude::Float64
@@ -16,31 +41,70 @@ struct KMLTrackPoint
     altitude::Float64
 end
 
+
+"""
+    KMLTrack()
+
+A KMLTrack is a collection of track points.
+
+`gx:Track` element is an extension of the OGC KML 2.2 standard and is supported in Google Earth 5.2 and later.
+"""
 struct KMLTrack
     points::Vector{KMLTrackPoint}
     KMLTrack() = new(Vector{KMLTrackPoint}[])
 end
 
+"""
+    KMLPlacemark()
+
+Placemark is a special kind of Feature. It contains all of the elements that belong to Feature, and it adds some elements that are specific to the Placemark element.
+
+See `Placemark` tag.
+"""
 struct KMLPlacemark
     track::KMLTrack
     KMLPlacemark() = new(KMLTrack())
 end
 
+
+"""
+    KMLDocument()
+
+A KML Document is a container for placemark.
+
+Be aware that `KMLDocument` is related to `kml` tag not to `Document` tag.
+"""
 struct KMLDocument
     placemark::KMLPlacemark
     KMLDocument() = new(KMLPlacemark())
 end
 
+"""
+    read_kml_file(fname) -> KMLDocument
+
+Read KML file from filename `fname`.
+"""
 function read_kml_file(fname)
     xdoc = parse_file(fname)
     return _parse_kml(xdoc)
 end
 
+"""
+    parse_kml_string(s) -> KMLDocument
+
+
+Parse GPX data from String `s`.
+"""
 function parse_kml_string(s)
     xdoc = parse_string(s)
     return _parse_kml(xdoc)
 end
 
+"""
+    _parse_kml(xdoc::XMLDocument) -> KMLDocument
+
+Parse `XMLDocument` and return a `KMLDocument`.
+"""
 function _parse_kml(xdoc::XMLDocument)
     kmls = root(xdoc)
 
